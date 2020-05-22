570b735c4598515b1ae43275c6d5156a90a7fc7f
Qupipab <54556157+Qupipab@users.noreply.github.com>
2020-05-22 17:36:25 +0300

    Add new route to leisure.vue and add leisure page

---
+++ front/src/views/leisure/leisure.map.vue
@@ -0,0 +1,54 @@
+<template>
+  <GoogleMapLoader
+    :map-config="mapConfig"
+    api-key="AIzaSyDuiiLqO-HTj8vP0POPrsBGJdWc7esHdOY"
+  >
+    // insert your google maps api key to render styled map
+    <template slot-scope="{ google, map }">
+      <GoogleMapMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :marker="marker"
+        :google="google"
+        :map="map"
+      />
+      <GoogleMapLine
+        v-for="line in lines"
+        :key="line.id"
+        :path.sync="line.path"
+        :google="google"
+        :map="map"
+      />
+    </template>
+  </GoogleMapLoader>
+</template>
+
+<script>
+import GoogleMapLoader from '@/components/google-map/GoogleMapLoader';
+import GoogleMapMarker from '@/components/google-map/GoogleMapMarker';
+import GoogleMapLine from '@/components/google-map/GoogleMapLine';
+
+import { mapSettings } from '@/constants/mapSettings';
+
+export default {
+  components: {
+    GoogleMapLoader,
+    GoogleMapMarker,
+    GoogleMapLine
+  },
+  data () {
+    return {
+      markers: [],
+      lines: []
+    };
+  },
+  computed: {
+    mapConfig () {
+      return {
+        ...mapSettings,
+        zoom: 13
+      };
+    }
+  }
+};
+</script>
---
+++ front/src/views/leisure/leisure.view.js
@@ -0,0 +1,72 @@
+import { VBreadcrumbs, VIcon, VBtn, VTextField, VSwitch } from 'vuetify/lib';
+import LeisureMap from './leisure.map.vue';
+import { MmtTextInput } from '@components/';
+export default {
+  components: {
+    MmtTextInput,
+    LeisureMap,
+    VBreadcrumbs,
+    VIcon,
+    VBtn,
+    VTextField,
+    VSwitch
+  },
+  data () {
+    return {
+      like: false,
+      showMap: true,
+      breadcrumbs: [
+        {
+          text: 'Досуг',
+          disabled: false,
+          href: 'breadcrumbs_dashboard'
+        },
+        {
+          text: 'Экскурсии',
+          disabled: false,
+          href: 'breadcrumbs_link_1'
+        },
+        {
+          text: 'Музеи и выставки',
+          disabled: true,
+          href: 'breadcrumbs_link_2'
+        }
+      ],
+      leisureType: [
+        'Активный отдых, экстрим',
+        'Музеи и выставки',
+        'Кинотеатры, театры',
+        'Красота, здоровье, Spa',
+        'Отдых для детей',
+        'Отдых для компании',
+        'Игровые клубы, квесты'
+      ],
+      places: [
+        {
+          title: 'Музей',
+          discription: 'Disc',
+          rating: 4.0,
+          state: 'Открыто',
+          price: 2
+        },
+        {
+          title: 'Музей2',
+          discription: 'Disc',
+          rating: 4.0,
+          state: 'Открыто',
+          price: 2
+        },
+        {
+          title: 'Музей3',
+          discription: 'Disc',
+          rating: 4.0,
+          state: 'Открыто',
+          price: 2
+        }
+      ]
+    };
+  },
+  mounted () {}
+};
+// heart-outline
+// heart
