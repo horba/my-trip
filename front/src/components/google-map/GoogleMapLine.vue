3a99cd4e7499c37822612ece8e4023270b47cd10
Qupipab <54556157+Qupipab@users.noreply.github.com>
2020-05-22 17:37:07 +0300

    Add google-map component

---
+++ front/src/components/google-map/GoogleMapLine.vue
@@ -0,0 +1,29 @@
+<script>
+import { LINE_PATH_CONFIG } from '@/constants/mapSettings';
+export default {
+  props: {
+    google: {
+      type: Object,
+      required: true
+    },
+    map: {
+      type: Object,
+      required: true
+    },
+    path: {
+      type: Array,
+      required: true
+    }
+  },
+  mounted () {
+    const { Polyline } = this.google.maps;
+    // eslint-disable-next-line no-new
+    new Polyline({
+      path: this.path,
+      map: this.map,
+      ...LINE_PATH_CONFIG
+    });
+  },
+  render () { return null; }
+};
+</script>
