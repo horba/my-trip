af5863fc644bbb11772d0bfd36f36aabca11597c
sanioooook <sahok6264@gmail.com>
2020-06-05 17:34:06 +0300

    fixed the value of the select (when restarting, the value was not displayed, only after selecting but before restarting)

--- front/src/components/mmt-locale-switcher/mmt-locale-switcher.component.js
+++ front/src/components/mmt-locale-switcher/mmt-locale-switcher.component.js
@@ -9,7 +9,7 @@ export default {
   },
   computed: {
     language () {
-      return this.$store.state.locale.language;
+      return  this.$store.state.locale.language;
     },
     locales () {
       return [
