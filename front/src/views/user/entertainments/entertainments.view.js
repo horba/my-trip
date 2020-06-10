287508c52abfc96be27196d57073bb45f26ad471
Qupipab <QupipabJob@gmail.com>
2020-06-10 19:31:12 +0300

    make base markup entertainments #72

---
+++ front/src/views/user/entertainments/entertainments.view.js
@@ -0,0 +1,46 @@
+import { MmtStepper } from '@components';
+
+export default {
+  components: {
+    MmtStepper
+  },
+  data () {
+    return {
+      places: [
+        {
+          photo: 'photo',
+          title: 'Title1',
+          rating: '5,0',
+          placeGraph: 'График работы',
+          placeState: 'Открыто',
+          description: 'Слово'
+        },
+        {
+          photo: 'photo',
+          title: 'Title2',
+          rating: '5,0',
+          placeGraph: 'График работы',
+          placeState: 'Открыто',
+          description: 'Слово'
+        },
+        {
+          photo: 'photo',
+          title: 'Title3',
+          rating: '5,0',
+          placeGraph: 'График работы',
+          placeState: 'Открыто',
+          description: 'Слово'
+        },
+        {
+          photo: 'photo',
+          title: 'Title4',
+          rating: '5,0',
+          placeGraph: 'График работы',
+          placeState: 'Открыто',
+          description: 'Слово'
+        }
+      ]
+    };
+  },
+  methods: {}
+};
---
+++ front/src/views/user/entertainments/entertainments.view.vue
@@ -0,0 +1,5 @@
+<template src="./entertainments.view.html"/>
+
+<script src="./entertainments.view.js"/>
+
+<style scoped src="./entertainments.view.css"/>
