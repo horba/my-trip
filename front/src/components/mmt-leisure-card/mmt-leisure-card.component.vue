1d110470e3c5df4cf648a1f62a41f661fcb65b1b
Qupipab <QupipabJob@gmail.com>
2020-06-11 03:49:40 +0300

    add leisure card component #86

---
+++ front/src/components/mmt-leisure-card/mmt-leisure-card.component.vue
@@ -0,0 +1,5 @@
+<template src="./mmt-leisure-card.component.html"></template>
+
+<script src="./mmt-leisure-card.component.js"></script>
+
+<style scoped src="./mmt-leisure-card.component.css"></style>
